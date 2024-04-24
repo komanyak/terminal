74be2c964bd8f2285f35fc71343923b8728cadde
Komanyak <you@example.com>
2024-04-25 02:59:13 +0300

    gotovo

--- terminal.c
+++ terminal.c
@@ -16,9 +16,10 @@ void handle_signal(int signum)
 {
     for (int i = 0; i < num_processes; ++i)
     {
-        kill(processes[i], SIGKILL); // Завершаем все запущенные процессы
+        kill(processes[i], SIGKILL); 
+        printf("Process %d killed\n", processes[i]);
     }
-    exit(0); // Выходим из программы
+    exit(0); 
 }
 
 // Функция для запуска нового процесса
@@ -27,15 +28,15 @@ void launch_process(char *args[])
     pid_t pid = fork(); // Создаем новый процесс
     if (pid == 0)
     {                                        // В дочернем процессе
-        printf("Child PID: %d\n", getpid()); // Выводим PID дочернего процесса
-        execvp(args[0], args);               // Заменяем текущий процесс на указанный исполняемый файл
-        perror("execvp");                    // В случае ошибки выводим сообщение
-        exit(1);                             // Завершаем дочерний процесс
+
+        execvp(args[0], args);               
+        perror("execvp");                    
+        exit(1);                             
     }
     else if (pid > 0)
     {                                                             // В родительском процессе
         processes[num_processes++] = pid;                         // Сохраняем PID нового процесса
-        printf("Parent PID: %d, Child PID: %d\n", getpid(), pid); // Выводим PID родительского и дочернего процессов
+        printf("Parent PID: %d, Child PID: %d\n\n", getpid(), pid); 
     }
     else
     {                   // В случае ошибки при создании нового процесса
@@ -52,30 +53,28 @@ int main()
 
     while (1)
     {                                       // Бесконечный цикл для ввода команд
-        printf("Custom Terminal $ ");       // Выводим приглашение к вводу
+        printf("\nCustom Terminal> ");       // Выводим приглашение к вводу
         fgets(input, sizeof(input), stdin); // Получаем ввод пользователя
         input[strcspn(input, "\n")] = '\0'; // Удаляем символ новой строки из ввода
 
-        char *token = strtok(input, " "); // Разбиваем ввод на токены (слова)
+        char *token = strtok(input, " "); // Разбиваем ввод на слова
         int i = 0;
-        while (token != NULL && i < MAX_ARGS - 1)
-        {                              // Пока не достигнут конец строки или максимальное количество аргументов
+        while (token != NULL && i < MAX_ARGS - 1) // Пока не достигнут конец строки или максимальное количество аргументов
+        {                              
             args[i++] = token;         // Сохраняем текущий токен в массив аргументов
             token = strtok(NULL, " "); // Получаем следующий токен
         }
         args[i] = NULL; // Устанавливаем последний аргумент в NULL, необходимо для execvp
 
-        if (args[0] != NULL)
-        { // Если введена какая-то команда
-            if (strcmp(args[0], "exit") == 0)
-            {                          // Если команда - "exit"
-                handle_signal(SIGINT); // Вызываем обработчик сигнала CTRL+C для завершения программы
-            }
-            else if (strcmp(args[0], "ls") == 0 || strcmp(args[0], "cat") == 0 ||
-                     strcmp(args[0], "nice") == 0 || strcmp(args[0], "killall") == 0 ||
-                     strcmp(args[0], "browser") == 0)
-            {
-                launch_process(args); // Запускаем процесс для введенной команды
+        if (args[0] != NULL) 
+        { 
+            if (strcmp(args[0], "browser") == 0) // Если введена команда "browser"
+            {   
+                int saved_stderr = dup(STDERR_FILENO);
+                freopen("/dev/null", "w", stderr);
+                char* browser[] = {"/usr/bin/yandex-browser", NULL};
+                launch_process(browser); 
+                dup2(saved_stderr, STDERR_FILENO);
             }
             else
             {
