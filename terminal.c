6cda3ae7c416d055c5efced6be369eff9754a738
komanyak <komanyak.alex@yandex.ru>
2024-04-25 00:33:44 +0300

    new

--- terminal.c
+++ terminal.c
@@ -2,112 +2,87 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 #include <signal.h>
 
-#define MAX_PROCESSES 10
+#define MAX_ARGS 10
 #define MAX_COMMAND_LENGTH 100
 
-// Структура для хранения информации о процессе
-typedef struct {
-    pid_t pid;
-    char command[MAX_COMMAND_LENGTH];
-} Process;
+int num_processes = 0;     // Хранит текущее количество запущенных процессов
+pid_t processes[MAX_ARGS]; // Массив для хранения PID запущенных процессов
 
-Process processes[MAX_PROCESSES];
-int num_processes = 0;
-
-// Обработчик сигнала SIGINT (Ctrl+C)
-void sigint_handler(int sig) {
-    printf("\nReceived SIGINT. Terminating all processes.\n");
-    // Завершаем все процессы
-    for (int i = 0; i < num_processes; ++i) {
-        kill(processes[i].pid, SIGKILL);
+// Обработчик сигнала CTRL+C
+void handle_signal(int signum)
+{
+    for (int i = 0; i < num_processes; ++i)
+    {
+        kill(processes[i], SIGKILL); // Завершаем все запущенные процессы
     }
-    exit(0);
+    exit(0); // Выходим из программы
 }
 
-int main() {
-    // Устанавливаем обработчик сигнала SIGINT
-    signal(SIGINT, sigint_handler);
+// Функция для запуска нового процесса
+void launch_process(char *args[])
+{
+    pid_t pid = fork(); // Создаем новый процесс
+    if (pid == 0)
+    {                                        // В дочернем процессе
+        printf("Child PID: %d\n", getpid()); // Выводим PID дочернего процесса
+        execvp(args[0], args);               // Заменяем текущий процесс на указанный исполняемый файл
+        perror("execvp");                    // В случае ошибки выводим сообщение
+        exit(1);                             // Завершаем дочерний процесс
+    }
+    else if (pid > 0)
+    {                                                             // В родительском процессе
+        processes[num_processes++] = pid;                         // Сохраняем PID нового процесса
+        printf("Parent PID: %d, Child PID: %d\n", getpid(), pid); // Выводим PID родительского и дочернего процессов
+    }
+    else
+    {                   // В случае ошибки при создании нового процесса
+        perror("fork"); // Выводим сообщение об ошибке
+    }
+}
 
-    char input[MAX_COMMAND_LENGTH];
+int main()
+{
+    char input[MAX_COMMAND_LENGTH]; // Буфер для ввода команды
+    char *args[MAX_ARGS];           // Массив аргументов команды
 
-    while (1) {
-        printf("Terminal> ");
-        fflush(stdout);
-        fgets(input, sizeof(input), stdin);
-        
-        // Удаление символа новой строки
-        input[strcspn(input, "\n")] = 0;
+    signal(SIGINT, handle_signal); // Устанавливаем обработчик сигнала CTRL+C
 
-        // Разбиваем входную строку на токены
-        char *token = strtok(input, " ");
+    while (1)
+    {                                       // Бесконечный цикл для ввода команд
+        printf("Custom Terminal $ ");       // Выводим приглашение к вводу
+        fgets(input, sizeof(input), stdin); // Получаем ввод пользователя
+        input[strcspn(input, "\n")] = '\0'; // Удаляем символ новой строки из ввода
 
-        while (token != NULL) {
-            if (strcmp(token, "ls") == 0) {
-                printf("Running 'ls' command...\n");
-                pid_t pid = fork();
-                if (pid == 0) {
-                    execlp("ls", "ls", (char *)NULL);
-                    exit(0);
-                } else {
-                    // Сохраняем информацию о процессе
-                    processes[num_processes].pid = pid;
-                    strcpy(processes[num_processes].command, "ls");
-                    num_processes++;
-                }
-            } else if (strcmp(token, "cat") == 0) {
-                printf("Running 'cat' command...\n");
-                pid_t pid = fork();
-                if (pid == 0) {
-                    execlp("cat", "cat", "example.txt", (char *)NULL);
-                    exit(0);
-                } else {
-                    processes[num_processes].pid = pid;
-                    strcpy(processes[num_processes].command, "cat");
-                    num_processes++;
-                }
-            } else if (strcmp(token, "nice") == 0) {
-                printf("Running 'nice' command...\n");
-                pid_t pid = fork();
-                if (pid == 0) {
-                    execlp("nice", "nice", "-n", "10", "ls", (char *)NULL);
-                    exit(0);
-                } else {
-                    processes[num_processes].pid = pid;
-                    strcpy(processes[num_processes].command, "nice");
-                    num_processes++;
-                }
-            } else if (strcmp(token, "killall") == 0) {
-                printf("Running 'killall' command...\n");
-                // Завершаем все процессы
-                for (int i = 0; i < num_processes; ++i) {
-                    kill(processes[i].pid, SIGKILL);
-                }
-                num_processes = 0;
-            } else if (strcmp(token, "exit") == 0) {
-                printf("Exiting terminal...\n");
-                // Завершаем все процессы
-                for (int i = 0; i < num_processes; ++i) {
-                    kill(processes[i].pid, SIGKILL);
-                }
-                exit(0);
-            } else {
-                // Если не найдено ключевое слово, пытаемся запустить программу
-                printf("Running '%s'...\n", token);
-                pid_t pid = fork();
-                if (pid == 0) {
-                    execlp(token, token, (char *)NULL);
-                    exit(0);
-                } else {
-                    // Сохраняем информацию о процессе
-                    processes[num_processes].pid = pid;
-                    strcpy(processes[num_processes].command, token);
-                    num_processes++;
-                }
-            }
-            token = strtok(NULL, " ");
+        char *token = strtok(input, " "); // Разбиваем ввод на токены (слова)
+        int i = 0;
+        while (token != NULL && i < MAX_ARGS - 1)
+        {                              // Пока не достигнут конец строки или максимальное количество аргументов
+            args[i++] = token;         // Сохраняем текущий токен в массив аргументов
+            token = strtok(NULL, " "); // Получаем следующий токен
         }
+        args[i] = NULL; // Устанавливаем последний аргумент в NULL, необходимо для execvp
+
+        if (args[0] != NULL)
+        { // Если введена какая-то команда
+            if (strcmp(args[0], "exit") == 0)
+            {                          // Если команда - "exit"
+                handle_signal(SIGINT); // Вызываем обработчик сигнала CTRL+C для завершения программы
+            }
+            else if (strcmp(args[0], "ls") == 0 || strcmp(args[0], "cat") == 0 ||
+                     strcmp(args[0], "nice") == 0 || strcmp(args[0], "killall") == 0 ||
+                     strcmp(args[0], "browser") == 0)
+            {
+                launch_process(args); // Запускаем процесс для введенной команды
+            }
+            else
+            {
+                launch_process(args); // Если введена другая команда, запускаем ее процесс
+            }
+        }
+        wait(NULL); // Ожидаем завершения дочернего процесса
     }
 
     return 0;
